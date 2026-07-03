739213c6282c56a15ae1a8e1162797829d10c18c
matiamorfati <mati@Matis-MacBook-Pro.local>
2026-07-03 20:42:56 +0200

    repair layout routing

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,13 +2,16 @@ import { Stack } from "expo-router";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export default function Layout() {
+export default function RootLayout() {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="pokemon" options={{ title: "Pokemon details" }} />
+        <Stack.Screen
+          name="pokemon/[name]"
+          options={{ title: "Pokemon details" }}
+        />
       </Stack>
     </QueryClientProvider>
   );
