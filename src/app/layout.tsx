57c3a2a5d07e9e95e124b71195913f29f6cc4041
matiamorfati <mati@Matis-MacBook-Pro.local>
2026-07-03 15:05:30 +0200

    wrap layout with queryProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
-import {Stack} from 'expo-router';
+import { Stack } from "expo-router";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Layout() {
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)"  options={{headerShown: false}}/>
-      <Stack.Screen name="pokemon" options={{title: 'Pokemon details'}} />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="pokemon" options={{ title: "Pokemon details" }} />
+      </Stack>
+    </QueryClientProvider>
   );
-}   
+}
