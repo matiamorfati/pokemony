65a3450673189a4cc34eda69444aec3bf37b1102
Mateusz Banasiak <121320678+matiamorfati@users.noreply.github.com>
2026-07-08 10:42:53 +0200

    Create tab bar UI

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from "expo-router";
 import { colors } from "../../storages/colors";
 import { useColorScheme } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 export default function Layout() {
   const colorScheme = useColorScheme();
   const backgroundColor =
@@ -14,12 +15,62 @@ export default function Layout() {
           backgroundColor,
         },
         headerTintColor: textColor,
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "gray",
       }}
     >
-      <Tabs.Screen name="pokemon" options={{ title: "Pokemon List" }} />
-      <Tabs.Screen name="map" options={{ title: "Map" }} />
-      <Tabs.Screen name="camera" options={{ title: "Camera" }} />
-      <Tabs.Screen name="favourite" options={{ title: "Favourite" }} />
+      <Tabs.Screen
+        name="pokemon"
+        options={{
+          title: "Pokémon List",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "list-sharp" : "list-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{
+          title: "Map",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "map" : "map-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: "Camera",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "camera" : "camera-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favourite"
+        options={{
+          title: "Favourite",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "heart" : "heart-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
--- src/app/(tabs)/camera.tsx
+++ src/app/(tabs)/camera.tsx
@@ -1,3 +1,13 @@
-export default function Camera() {
-  return <text>Camera</text>;
-}
+// import { useState, useEffect } from "react";
+// import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+// import { Camera, CameraType } from "expo-camera";
+
+// export default function CamerScreen() {
+//   const [type, setType] = useState<CameraType>(CameraType.back);
+
+//   return (
+//     <View>
+//       <Text>Camera</Text>
+//     </View>
+//   );
+// }
